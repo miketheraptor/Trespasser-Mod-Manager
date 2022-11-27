118bd973b3a7770c5e2b7d0e9e0e1f32540c8208
miketheraptor <michael.j.acosta@gmail.com>
2022-11-27 11:10:50 -0500

    Fixed Scrollbar
    Fixed scroll bar and minor code reformatting (line shortening)

--- tmm.py
+++ tmm.py
@@ -4,7 +4,7 @@ The GUI-based mod manager for Trespasser CE
 '''
 
 
-VERSION_NUMBER = 'v0.3.7'
+VERSION_NUMBER = 'v0.3.8'
 
 
 import logging
@@ -15,8 +15,10 @@ from configparser import ConfigParser
 import shutil
 
 
-logging.basicConfig(format='%(asctime)s - %(levelname)s:%(message)s',
-    datefmt='%Y-%m-%d %I:%M', level=logging.INFO)
+logging.basicConfig(
+    format='%(asctime)s - %(levelname)s:%(message)s',
+    datefmt='%Y-%m-%d %I:%M',
+    level=logging.INFO)
 
 
 def backup_trespasser_ini():
@@ -45,7 +47,9 @@ def dependency_validation():
     if os.path.exists('tp_mod.ini'):
         logging.info('dependency_validation says found tp_mod.ini')
     else:
-        messagebox.showerror('Trespasser Mod Manager - ERROR', 'tp_mod.ini not found. Make sure that you are using Trespasser CE and that TMM.exe has been placed in the same directory as your Trespasser CE exe.')
+        messagebox.showerror(
+            'Trespasser Mod Manager - ERROR',
+            'tp_mod.ini not found. Make sure that you are using Trespasser CE and that TMM.exe has been placed in the same directory as your Trespasser CE exe.')
         logging.critical('tp_mod.ini not found')
         raise SystemExit
     if os.path.exists('trespasser.ini'):
@@ -82,8 +86,6 @@ def set_active_mod(selected_mod, label):
     parser = ConfigParser()
     parser.read('tp_mod.ini')
     parser['FM']['ActiveFM'] = selected_mod
-    if parser.has_option('FM', 'UseRecommendedQuality'):
-        parser['FM']['UseRecommendedQuality'] = parser['FM']['UseRecommendedQuality'] # Preserves the user's original UseRecommendedQuality setting
     with open('tp_mod.ini', 'w') as new_config_file:
         parser.write(new_config_file)
     label.config(text='Active Mod: '+ selected_mod)
@@ -91,7 +93,7 @@ def set_active_mod(selected_mod, label):
 
 def get_installed_mods():
     '''
-    Checks the user's tp_mod.ini to determine the fmpath folder, then checks all directories in the fmpath folder to generate a list of which mods are installed.
+    Return a list of all installed mods in fmpath directory
     '''
     parser = ConfigParser()
     parser.read('tp_mod.ini')
@@ -179,7 +181,10 @@ class MainApplication:
 
         # ====== Create game launch button ======
 
-        launch_button = Button(self.master, text='Launch Trespasser CE', command=lambda: launch_game())
+        launch_button = Button(
+            self.master,
+            text='Launch Trespasser CE',
+            command=lambda: launch_game())
         launch_button.pack(side='top', padx=5, pady=5)
 
         # ====== Create notebook tabs ======
@@ -193,7 +198,9 @@ class MainApplication:
 
         # ====== Create mods_frame content ======
 
-        active_mod_text = Label(mods_frame, text=f'Active Mod: {self.active_mod}')
+        active_mod_text = Label(
+            mods_frame,
+            text=f'Active Mod: {self.active_mod}')
         active_mod_text.pack(side='top')
 
         installed_mods_listbox = Listbox(mods_frame, height=10)
@@ -202,14 +209,25 @@ class MainApplication:
         installed_mods_listbox.pack(expand='true', side='top', fill='both', padx=10, pady=5)
         installed_mods_listbox_scrollbar = Scrollbar(installed_mods_listbox)
         installed_mods_listbox_scrollbar.pack(side='right', fill='y', expand='false')
+        installed_mods_listbox.config(yscrollcommand=installed_mods_listbox_scrollbar.set)
+        installed_mods_listbox_scrollbar.config(command=installed_mods_listbox.yview)
         installed_mods_listbox.bind('<<ListboxSelect>>', self.set_selected_mod)
 
-        active_mod_changer_button = Button(mods_frame, text='Set Selected as Active Mod', command=lambda: set_active_mod(self.selected_mod, active_mod_text))
+        active_mod_changer_button = Button(
+            mods_frame,
+            text='Set Selected as Active Mod',
+            command=lambda: set_active_mod(self.selected_mod, active_mod_text))
         active_mod_changer_button.pack(side='top')
 
         self.quality_var = IntVar()
         self.quality_var.set(get_mod_quality_setting())
-        mod_quality_setting_toggle = Checkbutton(mods_frame, text='Use Recommended Quality Settings from Active Mod', variable=self.quality_var, onvalue=1, offvalue=0, command=set_mod_quality_setting)
+        mod_quality_setting_toggle = Checkbutton(
+            mods_frame,
+            text='Use Recommended Quality Settings from Active Mod',
+            variable=self.quality_var,
+            onvalue=1,
+            offvalue=0,
+            command=set_mod_quality_setting)
         mod_quality_setting_toggle.pack(side='bottom')
 
         # ====== Create ce_options_frame content ======
@@ -225,12 +243,20 @@ class MainApplication:
 
         self.options_var_quicksave = IntVar()
         self.options_var_quicksave.set(get_ceoptions_setting('General', 'EnableQuickSaveKey'))
-        options_ckbtn_quicksave = Checkbutton(general_label_frame, text='Set F9 as quicksave key', variable=self.options_var_quicksave, command=lambda: set_ceoptions_setting('General', 'EnableQuickSaveKey'))
+        options_ckbtn_quicksave = Checkbutton(
+            general_label_frame,
+            text='Set F9 as quicksave key',
+            variable=self.options_var_quicksave,
+            command=lambda: set_ceoptions_setting('General', 'EnableQuickSaveKey'))
         options_ckbtn_quicksave.grid(row=0, column=0, stick='w')
 
         self.options_var_dualstow = IntVar()
         self.options_var_dualstow.set(get_ceoptions_setting('General', 'EnableDualStow'))
-        options_ckbtn_dualstow = Checkbutton(general_label_frame, text='Enable stowing two items at a time', variable=self.options_var_dualstow, command=lambda: set_ceoptions_setting('General', 'EnableDualStow'))
+        options_ckbtn_dualstow = Checkbutton(
+            general_label_frame,
+            text='Enable stowing two items at a time',
+            variable=self.options_var_dualstow,
+            command=lambda: set_ceoptions_setting('General', 'EnableDualStow'))
         options_ckbtn_dualstow.grid(row=1, column=0, sticky='w')
 
         # Display Options Checkboxes
@@ -241,12 +267,20 @@ class MainApplication:
 
         self.options_var_water = IntVar()
         self.options_var_water.set(get_ceoptions_setting('DisplayDX9', 'WaterRefraction'))
-        options_ckbtn_water = Checkbutton(display_label_frame, text='Enable refractive water effect', variable=self.options_var_water, command=lambda: set_ceoptions_setting('DisplayDX9', 'WaterRefraction'))
+        options_ckbtn_water = Checkbutton(
+            display_label_frame,
+            text='Enable refractive water effect',
+            variable=self.options_var_water,
+            command=lambda: set_ceoptions_setting('DisplayDX9', 'WaterRefraction'))
         options_ckbtn_water.grid(row=0, column=0, stick='w')
 
         self.options_var_sky = IntVar()
         self.options_var_sky.set(get_ceoptions_setting('DisplayDX9', 'EnhancedSky'))
-        options_ckbtn_sky = Checkbutton(display_label_frame, text='Enable new sky rendering method', variable=self.options_var_sky, command=lambda: set_ceoptions_setting('DisplayDX9', 'EnhancedSky'))
+        options_ckbtn_sky = Checkbutton(
+            display_label_frame,
+            text='Enable new sky rendering method',
+            variable=self.options_var_sky,
+            command=lambda: set_ceoptions_setting('DisplayDX9', 'EnhancedSky'))
         options_ckbtn_sky.grid(row=1, column=0, sticky='w')
 
         # Render Options Checkboxes
@@ -257,7 +291,11 @@ class MainApplication:
 
         self.options_var_maxlod = IntVar()
         self.options_var_maxlod.set(get_ceoptions_setting('Render', 'ForceMaxObjectDetail'))
-        options_ckbtn_maxlod = Checkbutton(render_label_frame, text='Force Maximum Level of Detail', variable=self.options_var_maxlod, command=lambda: set_ceoptions_setting('Render', 'ForceMaxObjectDetail'))
+        options_ckbtn_maxlod = Checkbutton(
+            render_label_frame,
+            text='Force maximum level of detail',
+            variable=self.options_var_maxlod,
+            command=lambda: set_ceoptions_setting('Render', 'ForceMaxObjectDetail'))
         options_ckbtn_maxlod.grid(row=0, column=0, stick='w')
 
     def configure_gui(self):
@@ -273,7 +311,7 @@ class MainApplication:
     def set_selected_mod(self, event):
         for i in event.widget.curselection():
             selected_mod = event.widget.get(i)
-        logging.info('check_selected_mod says the selected mod is: ' + selected_mod)
+        logging.info(f'check_selected_mod says the selected mod is: {selected_mod}')
         self.selected_mod = selected_mod
 
     def ce_exe_settings_prompt(self):
@@ -286,22 +324,35 @@ class MainApplication:
         settings_window.resizable('False','False')
         self.master.eval(f'tk::PlaceWindow {str(settings_window)} center')
         settings_window.grab_set()
-        settings_label = Label(master=settings_window, justify='left', wraplength=400, text="Your Trespasser CE exe hasn't been configured yet.\nFind your Trespasser CE exe file, press save, and try again.")
+        settings_label = Label(
+            master=settings_window,
+            justify='left',
+            wraplength=400,
+            text="Your Trespasser CE exe hasn't been configured yet.\nFind your Trespasser CE exe file, press save, and try again.")
         settings_label.grid(column=0, row=0, columnspan=1, sticky='w', padx=10, pady=(10,0))
-        exe_path_label = Label(master=settings_window, text="Trespasser CE Exe Path:")
+        exe_path_label = Label(master=settings_window, text="Trespasser CE exe path:")
         exe_path_label.grid(column=0, row=1, sticky='w', padx=10, pady=(10,0))
         exe_path_label2 = Label(master=settings_window, width=60, relief='sunken')
         exe_path_label2.grid(column=0, row=2, sticky='w', padx=(10,0), pady=(0,10))
-        exe_path_button = Button(master=settings_window, text='...', command=lambda: self.get_exe_path(exe_path_label2))
+        exe_path_button = Button(
+            master=settings_window,
+            text='...',
+            command=lambda: self.get_exe_path(exe_path_label2))
         exe_path_button.grid(column=1, row=2, sticky='w', padx=(0, 10))
-        settings_save_button = Button(master=settings_window, text="Save", command=lambda: self.set_exe_path(settings_window))
+        settings_save_button = Button(
+            master=settings_window,
+            text="Save",
+            command=lambda: self.set_exe_path(settings_window))
         settings_save_button.grid(column=3, row=3, sticky='se', padx=10, pady=10)
 
     def get_exe_path(self, label):
         '''
         Opens file browser window for user selection of Tres CE exe path and updates label with path.
         '''
-        self.exe_path = filedialog.askopenfilename(initialdir='./', title='Select Trespasser CE EXE', filetypes=[('Trespasser CE','*.exe')])
+        self.exe_path = filedialog.askopenfilename(
+            initialdir='./',
+            title='Select Trespasser CE EXE',
+            filetypes=[('Trespasser CE','*.exe')])
         label.config(text=self.exe_path)
 
     def set_exe_path(self, window):
